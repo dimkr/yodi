014b6d56e946dbf94838ae247762e273cff2045d
Dima Krasner <dima@dimakrasner.com>
2020-07-20 23:39:53 +0300

    remove -pthread

--- client/yodi.c
+++ client/yodi.c
@@ -1,59 +1,9 @@
-#include <pthread.h>
+#include <time.h>
+#include <strings.h>
 #include <stdlib.h>
-#include <stdint.h>
-#include <signal.h>
-#include <string.h>
-
-#include <boydemdb.h>
+#include <sys/prctl.h>
 
 #include <yodi.h>
-
-struct client_args {
-	char **argv;
-	boydemdb db;
-	int argc;
-};
-
-static void *client_routine(void *p)
-{
-	const struct client_args *args = (const struct client_args *)p;
-
-	return (void *)(intptr_t)yodi_client(args->argc,
-	                                     args->argv);
-}
-
-int yodi_main(int argc, char *argv[])
-{
-	struct client_args args = {.argc = argc, .argv = argv};
-	pthread_t client;
-	sigset_t set;
-	int sig;
-
-	if ((sigemptyset(&set) < 0) ||
-	    (sigaddset(&set, SIGINT) < 0) ||
-	    (sigaddset(&set, SIGTERM) < 0))
-		return EXIT_FAILURE;
-
-	args.db = boydemdb_open("/tmp/x");
-	if (!args.db)
-		return EXIT_FAILURE;
-
-	if (pthread_create(&client, NULL, client_routine, &args) != 0) {
-		boydemdb_close(args.db);
-		return EXIT_FAILURE;
-	}
-
-	sigwait(&set, &sig);
-
-	pthread_cancel(client);
-	pthread_join(client, NULL);
-
-	boydemdb_close(args.db);
-
-	return EXIT_SUCCESS;
-}
-
-#include <sys/prctl.h>
 
 const char *yodi_now(void)
 {
@@ -70,8 +20,6 @@ const char *yodi_now(void)
 
 	return buf;
 }
-
-#include <sys/prctl.h>
 
 const char *yodi_progname(void)
 {
